9a0ebf9ab1e2ceb1547b8ccaa98087a67c3192a5
Junghwan Park <reserve.dev@gmail.com>
2019-05-10 08:32:32 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const classifier = knnClassifier.create();
+const webcamElement = document.getElementById('webcam');
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  await setupWebcam();
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-d').addEventListener('click', () => addExample(3));
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      const classes = ['A', 'B', 'C', 'D'];
+      document.getElementById('console').innerText = `
+        Prediction: ${classes[result.classIndex]}\n
+        Probability: ${result.confidences[result.classIndex] * 100}%
+      `;
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true },
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+app();
